4ebe0da31b195bb18f0bb19309c75b89804f0ff0
SergeiSSP <senkiv34@gmail.com>
2022-10-08 02:49:07 +0200

    hw16

--- teachers/models.py
+++ teachers/models.py
@@ -1,3 +1,60 @@
-from django.db import models
+from datetime import date
 
-# Create your models here.
+from django.core.validators import MinLengthValidator
+from django.db import models
+from faker import Faker
+
+VALID_DOMAIN_LIST = ('@gmail.com', '@yahoo.com', '@test.com')
+
+
+class Teacher(models.Model):
+    first_name = models.CharField(
+        max_length=50,
+        verbose_name='First name',
+        db_column='first_name',
+        validators=[MinLengthValidator(2, 'First name must be greater than 1 character')]
+    )
+    last_name = models.CharField(
+        max_length=50,
+        verbose_name='Last_name',
+        db_column='last_name',
+        validators=[MinLengthValidator(2, 'First name must be greater than 1 character')]
+    )
+    birthday = models.DateField(
+        default=date.today,
+        null=False,
+        blank=False,
+    )
+    mail = models.EmailField(
+        null=True,
+        blank=True,
+    )
+    phone = models.CharField(
+        max_length=13,
+        verbose_name='Phone number',
+        db_column='phone_number',
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+    class Meta:
+        db_table = 'teachers'
+    @classmethod
+    def generate_fake_data(cls, cnt):
+        f = Faker()
+
+        for _ in range(cnt):
+            first_name = f.first_name()
+            last_name = f.last_name()
+            mail = f'{first_name}.{last_name}{f.random.choice(VALID_DOMAIN_LIST)}'
+            birthday = f.date()
+            st = cls(first_name=first_name, last_name=last_name, mail=mail, birthday=birthday)
+            try:
+                st.full_clean()
+                st.save()
+            except:
+                print(f'Incorrect data{first_name}{last_name}{mail}{birthday}')
--- teachers/views.py
+++ teachers/views.py
@@ -1,3 +1,71 @@
+from django.http import HttpResponseRedirect, HttpResponse
+from django.middleware.csrf import get_token
 from django.shortcuts import render
 
-# Create your views here.
+from teachers.forms import CreateTeacherForm, UpdateTeacherForm
+from teachers.models import Teacher
+
+
+def get_teachers(request):
+    teachers = Teacher.objects.all()
+    return render(
+        request,
+        'teachers/list.html',
+        context={
+            'title': 'List of teachers',
+            'teachers': teachers
+        }
+    )
+
+
+def create_teacher(request):
+    if request.method == 'GET':
+        form = CreateTeacherForm()
+    elif request.method == 'POST':
+        form = CreateTeacherForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/teachers')
+    token = get_token(request)
+    html_form = f'''
+                <form method='post'>
+                <input type = 'hidden' name = 'csrfmiddlewaretoken' value = '{token}'>
+                    <table>
+                        {form.as_table()}
+                    </table>
+                    <input type="submit" value="Submit">
+                </form> 
+            '''
+    return HttpResponse(html_form)
+
+
+def update_teacher(request, teacher_id):
+    teacher = Teacher.objects.get(pk=teacher_id)
+    if request.method == 'GET':
+        form = UpdateTeacherForm(instance=teacher)
+    elif request.method == 'POST':
+        form = UpdateTeacherForm(request.POST, instance=teacher)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/teachers')
+
+    token = get_token(request)
+    html_form = f'''
+            <form method='post'>
+            <input type = 'hidden' name = 'csrfmiddlewaretoken' value = '{token}'>
+                <table>
+                    {form.as_table()}
+                </table>
+                <input type="submit" value="Update">
+            </form> 
+        '''
+    return HttpResponse(html_form)
+
+
+def detailed_teacher(request, teacher_id):
+    teacher = Teacher.objects.get(pk=teacher_id)
+    return render(
+        request,
+        'teachers/details.html',
+        {'teacher': teacher}
+    )

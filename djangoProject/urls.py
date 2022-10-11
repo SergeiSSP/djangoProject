fbf19f58c5ae2a490216d4065073a35d3f774447
SergeiSSP <senkiv34@gmail.com>
2022-10-11 16:05:56 +0200

    hw17

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -17,21 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 from core.views import index
-from groups.views import get_groups, create_group, detailed_group, update_group
 
-from teachers.views import get_teachers, create_teacher, update_teacher, detailed_teacher
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='home'),
     path('students/', include('students.urls')),
-    path('groups/', get_groups),
-    path('groups/create/', create_group),
-    path('groups/detail/<int:group_id>/', detailed_group),
-    path('groups/update/<int:group_id>/', update_group),
-    path('teachers/', get_teachers),
-    path('teachers/create/', create_teacher),
-    path('teachers/update/<int:teacher_id>/', update_teacher),
-    path('teachers/detail/<int:teacher_id>/', detailed_teacher),
-
+    path('teachers/', include('teachers.urls')),
+    path('groups/', include('groups.urls')),
 ]
